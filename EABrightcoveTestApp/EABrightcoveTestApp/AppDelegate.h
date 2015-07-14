a6213844d09a722ccb3240a0ab1e8f5f02219ef2
Egay <egfojas@voyagerinnovation.com>
2015-07-14 20:03:44 +0800

    Created initial project

---
+++ EABrightcoveTestApp/EABrightcoveTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EABrightcoveTestApp
+//
+//  Created by Edgar Allan Fojas on 7/14/15.
+//  Copyright (c) 2015 egay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EABrightcoveTestApp/EABrightcoveTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EABrightcoveTestApp
+//
+//  Created by Edgar Allan Fojas on 7/14/15.
+//  Copyright (c) 2015 egay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
