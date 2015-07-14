77fbaefc2707e135e100deaa338e155356af7574
Egay <egfojas@voyagerinnovation.com>
2015-07-14 20:23:51 +0800

    Added EAMainViewController

---
+++ EABrightcoveTestApp/EABrightcoveTestApp/EAMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAMainViewController.h
+//  EABrightcoveTestApp
+//
+//  Created by Edgar Allan Fojas on 7/14/15.
+//  Copyright (c) 2015 egay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAMainViewController : UIViewController
+
+@end
---
+++ EABrightcoveTestApp/EABrightcoveTestApp/EAMainViewController.m
@@ -0,0 +1,23 @@
+//
+//  EAMainViewController.m
+//  EABrightcoveTestApp
+//
+//  Created by Edgar Allan Fojas on 7/14/15.
+//  Copyright (c) 2015 egay. All rights reserved.
+//
+
+#import "EAMainViewController.h"
+
+@interface EAMainViewController ()
+
+@end
+
+@implementation EAMainViewController
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
